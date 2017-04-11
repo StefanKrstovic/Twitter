61a7c8fb74080c0f01c4c3478b5dffab98aa9e61
StefanKrstovic <stefan.krstovic@outlook.com>
2017-04-11 03:23:12 +0200

    Odradjen test za Twitter klasu

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -34,7 +34,10 @@ public class Twitter {
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
-		tp.setKorisnik("korisnik");
+		/**tp.setKorisnik("korisnik");
+		 * bug 005 -> treba korisnik a ne "korisnik". Napravljena metoda koja resava ovo
+		 */
+		tp.setKorisnik(korisnik);
 		tp.setPoruka(poruka);
 
 		// Poruka se unosi u listu na kraj
@@ -44,8 +47,8 @@ public class Twitter {
 	/**
 	 * Metoda koja u obliku niza objekata TwitterPoruka vraca sve poruke koje sadrze prosledjeni tag
 	 * @param maxBroj najveci broj poruka koje je dozvoljeno vratiti
-	 * @param tag 
-	 * @return
+	 * @param tag String koji treba da se nalazi unutar neke poruke
+	 * @return odgovarajuce poruke kao niz objekata klase TwitterPoruka
 	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
@@ -64,7 +67,10 @@ public class Twitter {
 		for (int i = 0; i < poruke.size(); i++)
 			if (poruke.get(i).getPoruka().indexOf(tag) != -1)
 				if (brojac < maxBroj) {
-					rezultat[brojac + 1] = poruke.get(i);
+					/**rezultat[brojac + 1] = poruke.get(i);
+					 * Bug 004 -> null pointer. Uhvatio ga je testVratiPoruke
+					 */
+					rezultat[brojac] = poruke.get(i);
 					brojac++;
 				} else
 					break;
---
+++ Twitter/test/com/twitter/TwitterTest.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package com.twitter;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.twitter.poruke.TwitterPoruka;
+
+/**
+ * @author Stefan Krstovic
+ *
+ */
+public class TwitterTest {
+	
+	private Twitter t;
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		t=new Twitter();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		t=null;
+	}
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#vratiSvePoruke()}.
+	 */
+	@Test
+	public void testVratiSvePoruke() {
+		t.vratiSvePoruke().clear();
+		t.unesi("Ran", "dd");
+		t.unesi("Rand", "om");
+		
+		if(t.vratiSvePoruke().get(0).getKorisnik().equals("Ran") && t.vratiSvePoruke().get(0).getPoruka().equals("dd") && t.vratiSvePoruke().get(1).getKorisnik().equals("Rand") && t.vratiSvePoruke().get(1).getPoruka().equals("om"))assertEquals(1,1);
+		else assertEquals(1,0);
+	}
+	
+	public void bug005squasher(){
+		t.vratiSvePoruke().clear();
+		t.unesi("k1", "p1");
+		t.unesi("k2", "p2");
+		
+		if(t.vratiSvePoruke().get(0).getKorisnik().equals(t.vratiSvePoruke().get(1).getKorisnik()))assertEquals(1,0);
+		else assertEquals(1,1);
+	}
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#unesi(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testUnesi() {
+		t.vratiSvePoruke().clear();
+		t.unesi("John", "Doe");
+		
+		assertEquals("John"+"Doe", t.vratiSvePoruke().get(0).getKorisnik()+t.vratiSvePoruke().get(0).getPoruka());
+	}
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#vratiPoruke(int, java.lang.String)}.
+	 */
+	@Test
+	public void testVratiPoruke() {
+		t.unesi("Ime", "Poruka");
+		t.unesi("Jos jedno ime", "Pooruka");
+		t.unesi("Novo ime", "ruka");
+		
+		TwitterPoruka[] t2 = t.vratiPoruke(5, "ruka");
+		assertEquals("Ime" + "Poruka" + "Jos jedno ime" + "Pooruka"+"Novo ime" + "ruka", t2[0].getKorisnik()+t2[0].getPoruka()+t2[1].getKorisnik()+t2[1].getPoruka()+t2[2].getKorisnik()+t2[2].getPoruka());
+	}
+
+}
